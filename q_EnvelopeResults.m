8fd5d381f0f28116cd9ad69cdb4f00dfb3f8f684
Jacob Kraniak <113630934+jacob-kraniak@users.noreply.github.com>
2026-03-13 16:43:50 -0400

    Update q_EnvelopeResults.m
    added comments for data source instructions

--- q_EnvelopeResults.m
+++ q_EnvelopeResults.m
@@ -1,4 +1,7 @@
 // q_EnvelopeResults
+// This query directly retrieves your source data from an Excel Table, here named "tbl_EnvelopeResults". 
+// All this needs to work is a single column with one "@domain.com" per row.
+
 let
     Source = Excel.CurrentWorkbook(){[Name="tbl_EnvelopeResults"]}[Content],
     #"Changed Type" = Table.TransformColumnTypes(Source, {{"SenderFromDomain", type text}})
